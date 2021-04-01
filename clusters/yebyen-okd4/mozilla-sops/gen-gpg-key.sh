f546a423771c7694f66a76430de7f5cbd3abd63d
Kingdon Barrett <kingdon@weave.works>
2021-04-01 10:53:37 -0400

    add gpg key (public key)

---
+++ clusters/yebyen-okd4/mozilla-sops/gen-gpg-key.sh
@@ -0,0 +1,13 @@
+export KEY_NAME="okd4.csh.rit.edu"
+export KEY_COMMENT="flux secrets - yebyen-okd4"
+
+gpg --batch --full-generate-key <<EOF
+%no-protection
+Key-Type: 1
+Key-Length: 4096
+Subkey-Type: 1
+Subkey-Length: 4096
+Expire-Date: 0
+Name-Comment: ${KEY_COMMENT}
+Name-Real: ${KEY_NAME}
+EOF
